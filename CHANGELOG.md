76b8551e573281bf80eda25bd65915c13c3d81a5
Bojan <bojan.komljenovic@infinum.hr>
2021-04-12 15:06:37 +0200

    Update Gradle to 7.0. Use version catalogs.

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+Changelog
+=========
+
+## Version 1.0.0
+
+_2021-04-12_
+
+Initial release.
--- localian/src/main/kotlin/com/infinum/localian/extensions/Activity.kt
+++ localian/src/main/kotlin/com/infinum/localian/extensions/Activity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.pm.PackageManager
 import android.content.pm.PackageManager.GET_META_DATA
 
+@Suppress("PrintStackTrace")
 internal fun Activity.resetTitle() =
     try {
         val info = packageManager.getActivityInfo(componentName, GET_META_DATA)
